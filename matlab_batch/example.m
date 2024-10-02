e059c8c647644d1a4a2763a37b454bdc5c511c4c
yjiang1234 <146904245+yjiang1234@users.noreply.github.com>
2024-10-02 17:01:50 -0500

    Update example.m

--- matlab_batch/example.m
+++ matlab_batch/example.m
@@ -1,11 +1,11 @@
 % Image size and parameters
-image_width = 128; % In Pixals 
+image_width = 128; % In Pixels 
 image_height = 128;
 num_spots = 9; % Number of fluorescent spots
 spot_radius = 8; % Average radius of the fluorescnt sports
 spot_intensity_range = [1000, 3000];  % Intensity range of the spots
-min_distance_between_spots = 40; %Minimum distance to avoid overlap
-noise_level = 100; %set the camera noise level
+min_distance_between_spots = 40; % Minimum distance to avoid overlap
+noise_level = 100; % set the camera noise level
 
 
 file_name_for_save_fluorescent = 'fluorescent_image.png';
